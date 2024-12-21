a42eea2b8eee990eee594f24110601c008d6a4b4
Nayan Das <nayan010das@gmail.com>
2024-12-21 15:18:29 +0530

    first commit

---
+++ src/components/commom-layout/index.js
@@ -0,0 +1,13 @@
+
+
+function CommonLayout({children}) {
+  return (
+    <div className="mx-auto max-w-7xl p-6 lg:px-8">
+        {/* Header comp */}
+
+        
+    </div>
+  )
+}
+
+export default CommonLayout
